791a7351ec594f04e27fc41dbfd23f59528dafa1
alfmorais <alfredneto@hotmail.com>
2024-08-06 12:18:20 -0300

    feat: ContaTerminal to simulate bank account

---
+++ src/ContaTerminal.java
@@ -0,0 +1,55 @@
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public ContaTerminal() {}
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        String mensagem = "Olá " + this.getNomeCliente() + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + this.getAgencia() + ", conta " + this.getNumero() + " e seu saldo "
+                + this.getSaldo() + " já está disponível para saque.";
+        return mensagem;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ContaTerminal conta = new ContaTerminal();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        conta.setNumero(scanner.nextInt());
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        conta.setAgencia(scanner.next());
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        scanner.nextLine();
+        conta.setNomeCliente(scanner.nextLine());
+
+        System.out.print("Por favor, digite o saldo: ");
+        conta.setSaldo(scanner.nextDouble());
+
+        System.out.println(conta);
+        scanner.close();
     }
 }
